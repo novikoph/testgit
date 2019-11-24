ac511b2a81384d891817651d825b4f3cf9ce9902
Aleksandr Novikov <novikoph@gmail.com>
2019-11-24 21:47:38 +0300

    Loops and functions

---
+++ func_doc.py
@@ -0,0 +1,15 @@
+def printMax(x, y):
+    '''Выводит максимальное из двух чисел.
+
+Оба значения должны быть целыми числами'''
+    x = int(x)
+    y = int(y)
+
+    if x > y:
+        print(x, " набольшее")
+    else:
+        print(y, "наибольшее")
+
+printMax(3, 5)
+print(printMax.__doc__)
+help(printMax)
---
+++ func_param.py
@@ -0,0 +1,14 @@
+def printMax(a, b):
+    if a > b:
+        print(a, "максимально")
+    elif a == b:
+        print(a, "равно", b)
+    else:
+        print(b, "максимально")
+
+printMax(3, 4)
+
+x = 3
+y = 3
+
+printMax(x, y)
