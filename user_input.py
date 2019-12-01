1c4126beeeb24e8dc996760df8199adfa18b5b23
Aleksandr Novikov <novikoph@gmail.com>
2019-12-01 22:21:10 +0300

    Input n files

---
+++ user_input.py
@@ -0,0 +1,14 @@
+def reverse(text):
+    return text[::-1]
+
+def is_palindrome(text):
+    return text == reverse(text)
+
+something = input('Введите текст: ')
+something = something.replace(" ", "")
+something = something.lower()
+
+if (is_palindrome(something)):
+    print('Да, это палиндром.')
+else:
+    print('Нет, это не палиндром')
