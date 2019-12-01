1258575f867ed6cd166cc358c30faeb19ca4ac22
Aleksandr Novikov <novikoph@gmail.com>
2019-12-01 13:36:48 +0300

    Classes

--- inherit_abc.py
+++ inherit_abc.py
@@ -1,36 +1,40 @@
-class SchoolMember:
+from abc import *
+
+class SchoolMember(metaclass=ABCMeta):
     '''Представляет любого человека в школе.'''
     def __init__(self, name, age):
         self.name = name
         self.age = age
         print('(Создан SchoolMember: {0})'.format(self.name))
+
+    @abstractmethod
     def tell(self):
         '''Вывести информацию.'''
         print('Имя: "{0}" Возраст: "{1}"'.format(self.name, self.age), end=' ')
 
 class Teacher(SchoolMember):
-    '''Представляет проподавателя.'''
+    '''Представляет преподавателя.'''
     def __init__(self, name, age, salary):
         SchoolMember.__init__(self, name, age)
         self.salary = salary
-        print('(Создан Teacher: {0})'.format(self.name))
+        print('Создан Teacher: {0}'.format(self.name))
 
     def tell(self):
         SchoolMember.tell(self)
-        print('Зарплата: "{0:d}"'.format(self.salary))
+        print('Зарплата: "{0}"'.format(self.salary))
 
 class Student(SchoolMember):
     '''Представляет студента.'''
     def __init__(self, name, age, marks):
         SchoolMember.__init__(self, name, age)
         self.marks = marks
-        print('(Создан Student: {0})'.format(self.name))
+        print('Создан Student: {0}'.format(self.name))
 
     def tell(self):
         SchoolMember.tell(self)
-        print('Оценки: "{0:d}"'.format(self.marks))
+        print('Оценки :"{0}"'.format(self.marks))
 
-t = Teacher('Mrs. Shrividya', 40, 30000)
+t = Teacher('Mrs. Shividya', 40, 30000)
 s = Student('Koph', 29, 99)
 
 print()
@@ -38,3 +42,4 @@ print()
 members = [t, s]
 for member in members:
     member.tell() # работает как для преподавателя, так и для студента
+
