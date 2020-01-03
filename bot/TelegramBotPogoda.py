261ec7ff3c87516a0a81d32f9df635d5eef9fafb
Aleksandr Novikov <novikoph@gmail.com>
2020-01-03 19:45:44 +0300

    Bot

--- bot/TelegramBotPogoda.py
+++ bot/TelegramBotPogoda.py
@@ -1,10 +1,8 @@
 import telebot
 import pyowm
-from telebot import apihelper
 
 owm = pyowm.OWM('d41a733192602688c50263e6365382f5', language="ru")
 bot = telebot.TeleBot("957572159:AAGW6A3my3CI9k6bh4G4syoP2dCxfDLRR8M")
-
 
 
 @bot.message_handler(content_types=['text'])
@@ -15,6 +13,7 @@ def send_echo(message):
     answer = "Повелитель, в городе " + message.text + " сейчас " + w.get_detailed_status() + "\n"
     answer += "А температура " + str(temp) + "\n"
     bot.send_message(message.chat.id, answer)
+    bot.reply_to(message, message.text)
 
 
-bot.polling(none_stop=True)
+bot.polling(none_stop=True, timeout=10)
